c4ceef8d67502b9abe4e04cdc7fed2d1ec31ee45
Ekatha12 <74724806+Ekatha12@users.noreply.github.com>
2022-03-02 04:47:22 +0530

    Create MainActivity.java

---
+++ Ex13/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.ex13;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+Button b1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button b1=(Button)findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i= new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(i);
+            }
+        });
+    }
