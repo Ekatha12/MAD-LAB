4d5c24e0f3fbfecf7df3c926c41772187735b9ad
Ekatha12 <74724806+Ekatha12@users.noreply.github.com>
2022-03-02 05:06:48 +0530

    Create MainActivity.java

---
+++ Ex11/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.check_box;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+    ProgressBar pb;
+    int counter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        prog();
+    }
+
+    public void prog() {
+        pb = (ProgressBar) findViewById(R.id.progressBar);
+        final Timer t = new Timer();
+        TimerTask tt = new TimerTask() {
+            @Override
+            public void run() {
+                counter++;
+                pb.setProgress(counter);
+                if (counter == 100) {
+                    t.cancel();
+                }
+            }
+        };
+        t.schedule(tt, 0, 100);
+    }
+}
